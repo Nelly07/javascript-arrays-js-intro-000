d1d47764ebc85a47103e64af2ecbaa65ba5bb9f9
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-06 21:01:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,8 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 
 
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(1);
-  return chocolateBars;
+  return chocolateBars.slice(1);
   }
 
 
